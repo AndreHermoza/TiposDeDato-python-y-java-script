d821f580320d266950f91bf1db50ddd9dc21b7c6
AndreHermoza <167846400+AndreHermoza@users.noreply.github.com>
2024-05-09 22:59:20 -0500

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,26 @@
+//////Java script
+//tipo int
+function myFunction1() {
+var num = 8
+var num2 = 10
+var suma = num+num2
+console.log("La suma de ambos valores es:", suma )
+
+}
+
+//boleano
+function myFunction2() {
+var num = 12
+var num2 = 10
+var comp = num<num2
+if(num<num2){console.log(num,"es menor que", num2)}
+else{console.log(num, "es mayor que", num2)}
+console.log(comp)
+}
+
+//lenght
+function myFunction() {
+var palabra = "Conmoción"
+var bites = palabra.length
+console.log("Su palabra tiene",bites, "letras")
+}
---
+++ datos.py
@@ -0,0 +1,13 @@
+##tipo int
+num = int(input("Ingrese su número: "))
+print("su número es ",num)
+
+
+##boleano
+num1 = int(input("Ingrese su primer numero: "))
+num2 = int(input("Ingrese su segundo numero: "))
+if num1 < num2:
+    print(num1, "es menor que",num2)
+else:
+    print(num1, "es mayor que",  num2)
+
